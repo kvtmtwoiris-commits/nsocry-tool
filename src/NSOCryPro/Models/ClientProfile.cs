0661215aab16fef827ca52adf8240026bcd0ad46
kvtmtwoiris-commits <kvtmtwoiris@gmail.com>
2026-09-11 22:53:57 +0700

    Add src/NSOCryPro/Models/ClientProfile.cs

---
+++ src/NSOCryPro/Models/ClientProfile.cs
@@ -0,0 +1,12 @@
+namespace NSOCryPro.Models;
+
+public sealed class ClientProfile
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public bool Selected { get; set; } = true;
+    public string Account { get; set; } = string.Empty;
+    public string CharacterName { get; set; } = string.Empty;
+    public string Server { get; set; } = "Bokken";
+    public bool AutoRestart { get; set; } = true;
+}
+
