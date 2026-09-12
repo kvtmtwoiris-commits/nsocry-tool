2ca12cd1be6ffa8c377e465ad2078e1c12094042
kvtmtwoiris-commits <kvtmtwoiris@gmail.com>
2026-09-12 11:43:16 +0700

    Add secure auto login: update src/NSOCryPro/Models/ClientProfile.cs

--- src/NSOCryPro/Models/ClientProfile.cs
+++ src/NSOCryPro/Models/ClientProfile.cs
@@ -5,8 +5,9 @@ public sealed class ClientProfile
     public Guid Id { get; set; } = Guid.NewGuid();
     public bool Selected { get; set; } = true;
     public string Account { get; set; } = string.Empty;
+    public string EncryptedPassword { get; set; } = string.Empty;
     public string CharacterName { get; set; } = string.Empty;
     public string Server { get; set; } = "Bokken";
+    public bool AutoLogin { get; set; } = true;
     public bool AutoRestart { get; set; } = true;
 }
-
